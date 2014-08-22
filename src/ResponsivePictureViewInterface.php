a0bb6dfa7828a746c2d4b18692a8b8f153bd7f7b
Carsten Witt <carsten.witt@gmail.com>
2014-08-22 14:26:48 +0200

    Added invoke method and RP interceptors

--- src/ResponsivePictureViewInterface.php
+++ src/ResponsivePictureViewInterface.php
@@ -5,4 +5,24 @@ interface ResponsivePictureViewInterface {
 
     public function __toString();
 
+    /**
+     * @param  ResponsivePictureInterface $picture
+     * @return string
+     */
+    public function __invoke( ResponsivePictureInterface $picture );
+
+
+    /**
+     * @param  ResponsivePictureInterface $picture
+     * @return ResponsivePictureViewInterface Fluid Interface
+     */
+    public function setPicture( ResponsivePictureInterface $picture );
+
+
+    /**
+     * @return ResponsivePictureInterface
+     */
+    public function getPicture( );
+
+
 }
