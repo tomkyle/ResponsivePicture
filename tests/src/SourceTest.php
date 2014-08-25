70154bd94b195b9662254f696516672f6be5c83c
Carsten Witt <carsten.witt@gmail.com>
2014-08-25 21:27:24 +0200

    Added some Assertions

--- tests/src/SourceTest.php
+++ tests/src/SourceTest.php
@@ -7,18 +7,22 @@ class SourceTest extends \PHPUnit_Framework_TestCase {
 
     /**
      * @dataProvider provideVariousCtorParams
+     *
+     * @covers \tomkyle\ResponsivePicture\Source::__construct
      */
     public function testDefaultInstantiation( $media, $srcset)
     {
         $source = new Source( $media, $srcset);
         $this->assertInstanceOf('\tomkyle\ResponsivePicture\Source', $source);
-        $this->assertEquals( $source->media,  $media );
-        $this->assertEquals( $source->srcset, $srcset );
+        $this->assertEquals( $source->media,   $media );
+        $this->assertEquals( $source->srcset,  $srcset );
     }
 
 
     /**
      * @dataProvider provideVariousCtorParamsWithGivenType
+     *
+     * @covers \tomkyle\ResponsivePicture\Source::__construct
      */
     public function testInstantiationWithGivenType( $media, $srcset, $type )
     {
@@ -27,6 +31,43 @@ class SourceTest extends \PHPUnit_Framework_TestCase {
         $this->assertEquals( $source->media,  $media );
         $this->assertEquals( $source->srcset, $srcset );
         $this->assertEquals( $source->type,   $type );
+    }
+
+    /**
+     * @dataProvider provideVariousCtorParamsWithGivenType
+     *
+     * @covers \tomkyle\ResponsivePicture\Source::getMedia
+     * @covers \tomkyle\ResponsivePicture\Source::getSrcset
+     * @covers \tomkyle\ResponsivePicture\Source::getType
+     */
+    public function testGetterInterceptors( $media, $srcset, $type )
+    {
+        $source = new Source( $media, $srcset, $type);
+        $this->assertInstanceOf('\tomkyle\ResponsivePicture\Source', $source);
+        $this->assertEquals( $source->getMedia(),  $media );
+        $this->assertEquals( $source->getSrcset(), $srcset );
+        $this->assertEquals( $source->getType(),   $type );
+    }
+
+
+
+
+    /**
+     * @dataProvider provideVariousCtorParamsWithGivenType
+     *
+     * @covers \tomkyle\ResponsivePicture\Source::jsonSerialize
+     */
+    public function testJsonExport( $media, $srcset, $type )
+    {
+        $source = new Source( $media, $srcset, $type);
+
+        $json_source         = json_encode( $source );
+        $decoded_json_source = json_decode( $json_source );
+
+        $this->assertInstanceOf('\StdClass', $decoded_json_source);
+        $this->assertEquals( $decoded_json_source->media,  $media );
+        $this->assertEquals( $decoded_json_source->srcset, $srcset );
+        $this->assertEquals( $decoded_json_source->type,   $type );
     }
 
 
