b4f3b44452bcf06d5e751186acae28a4e0f9f915
Carsten Witt <carsten.witt@gmail.com>
2014-08-25 22:06:30 +0200

    Added more test

--- tests/src/ResponsivePictureTest.php
+++ tests/src/ResponsivePictureTest.php
@@ -3,6 +3,7 @@ namespace tests;
 
 use \tomkyle\ResponsivePicture\MinWidthSource;
 use \tomkyle\ResponsivePicture\ResponsivePicture;
+use \tomkyle\ResponsivePicture\SourceInterface;
 
 class ResponsivePictureTest extends \PHPUnit_Framework_TestCase {
 
@@ -18,5 +19,40 @@ class ResponsivePictureTest extends \PHPUnit_Framework_TestCase {
         $this->assertInstanceOf('\tomkyle\ResponsivePicture\ResponsivePicture', $this->object);
     }
 
+    /**
+     * @dataProvider provideSomeSourceInterfaceMocks
+     */
+    public function testAddingSource( $source_mock)
+    {
+        $this->object->add( $source_mock );
+        $this->assertSame( $source_mock, array_pop($this->object->sources) );
+    }
+
+    public function testGetIterator( )
+    {
+        $this->object->sources = [];
+        $this->object->add( $this->newSourceInterfaceMock() );
+        $this->object->add( $this->newSourceInterfaceMock() );
+        $this->object->add( $this->newSourceInterfaceMock() );
+
+        $iterator = $this->object->getIterator();
+
+        $this->assertInstanceOf('\ArrayIterator', $iterator);
+        $this->assertEquals(3, $iterator->count());
+    }
+
+    public function newSourceInterfaceMock()
+    {
+        return $this->getMock( '\tomkyle\ResponsivePicture\SourceInterface' );
+    }
+
+    public function provideSomeSourceInterfaceMocks()
+    {
+        return array(
+            array( $this->newSourceInterfaceMock() ),
+            array( $this->newSourceInterfaceMock() ),
+            array( $this->newSourceInterfaceMock() )
+        );
+    }
 
 }
